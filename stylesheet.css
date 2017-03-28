29f2b240688e984269b86dae8143519ce0f828a7
tdd982 <tdd198@gmail.com>
2017-03-28 17:38:10 -0500

    added index.html

---
+++ stylesheet.css
@@ -0,0 +1,8 @@
+ $(document).ready(function()    {
+    $('div').height("200px");
+    $('div').width("200px");
+    $('div').css("border-radius", "10px");
+    $('div').click(function(){
+      $('div').css("background-color", "red");
+    })
+});
